63060fd76238c22bfcb0c95830eee208faef384d
paulabssantos <paulablsantos7@gmail.com>
2023-08-17 10:58:30 -0400

    feat: rota de ponto a ponto

--- src/shared/routes/productAnalysis/pointToPoint.routes.ts
+++ src/shared/routes/productAnalysis/pointToPoint.routes.ts
@@ -1,19 +1,25 @@
 import { CreatePointToPointController } from "@modules/productAnalysis/useCases/createPointToPoint/CreatePointToPointController";
-import { UploadFile } from "@shared/middlewares/MulterFileMiddleware";
-import { EnsureAnalyst } from "@shared/middlewares/ensureAnalyst";
-import { EnsureAuthenticated } from "@shared/middlewares/ensureAuthenticate";
 import { Router } from "express";
 import multer from "multer";
-import path from "path"; 
+import path from "path";
+import { EnsureAnalyst } from "@shared/middlewares/ensureAnalyst";
+import { EnsureAuthenticated } from "@shared/middlewares/ensureAuthenticate";
+import { UploadFile } from "@shared/middlewares/MulterFileMiddleware";
 
 const createPointToPointController = new CreatePointToPointController();
-const pointToPointRouter = Router()
-const uploadFile = new UploadFile(path.resolve(process.cwd(), 'uploads','pointToPoint'));
-const upload = multer(uploadFile.getConfig)
+const pointToPointRouter = Router();
+const uploadFile = new UploadFile(
+  path.resolve(process.cwd(), "uploads", "pointToPoint"),
+);
+const upload = multer(uploadFile.getConfig);
 
-pointToPointRouter.use(EnsureAuthenticated)
-pointToPointRouter.use(EnsureAnalyst)
+pointToPointRouter.use(EnsureAuthenticated);
+pointToPointRouter.use(EnsureAnalyst);
 
-pointToPointRouter.post("/",upload.single("file"),createPointToPointController.handle)
+pointToPointRouter.post(
+  "/",
+  upload.single("file"),
+  createPointToPointController.handle,
+);
 
-export { pointToPointRouter }
+export { pointToPointRouter };
